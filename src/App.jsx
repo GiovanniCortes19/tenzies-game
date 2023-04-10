ab816325352cb06206b217e8cb1d1a8874304a94
GiovanniCortes <giocortes19@gmail.com>
2023-04-10 17:00:35 -0400

    initialized state for dice numbers and mapped over the array to generate Die elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import Die from "./Die"
+import { useState } from "react"
 
 
 function App() {
 
+  const [diceNumbers, setDiceNumbers] = useState(allNewDice())
+
+  function allNewDice(){
+    const diceArray = [];
+    for (let i = 0; i < 10; i++){
+      diceArray.push(Math.ceil(Math.random()*6))
+    }
+    return diceArray
+  }
+
+  const dice = diceNumbers.map(value => {
+    return <Die value={value}/>
+  })
+
   return (
     <main className="main">
       <div className="dice-container">
-        <Die value={3}/>
-        <Die value={3}/>
-        <Die value={3}/>
-        <Die value={3}/>
-        <Die value={3}/>
-        <Die value={3}/>
-        <Die value={3}/>
-        <Die value={3}/>
-        <Die value={3}/>
-        <Die value={3}/>
+        {dice}
       </div>
     </main>
   )
