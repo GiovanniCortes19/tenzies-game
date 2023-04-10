c8a20ca72a0983ad3ad7a6118e366fd0b1b5c4bc
GiovanniCortes <giocortes19@gmail.com>
2023-04-10 11:28:51 -0400

    initialized files for tenzies react-app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Tenzies Game!</h1>
+      <p className="read-the-docs">
+        This will be a tenzies game project
+      </p>
+    </div>
+  )
+}
+
+export default App
