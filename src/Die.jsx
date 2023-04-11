eaa51aa909dd57b8c3c2daf0dc31741ac9038300
GiovanniCortes <giocortes19@gmail.com>
2023-04-11 14:51:56 -0400

    converted dice to have dots instead of numbers

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react"
 
 export default function Die(props){
+
+    const [dots, setDots] = useState(Array(props.value).fill('dot'))
+
     const styles = {
         backgroundColor: props.isHeld ? '#59E391' : 'white'
     }
 
+    const diceDots = dots.map(dot => <span className="dot"></span>)
+
     return (
-        <div onClick={props.holdDice} className="die" style={styles}>
-            <h2 className="die-num">{props.value}</h2>
+        <div onClick={props.holdDice} className={`die die-${props.value}`} style={styles}>
+            {diceDots}
+            {/* <h2 className="die-num">{props.value}</h2> */}
         </div>
     )
 }
